8a1b56d24068ac5635f616b2d783be8780e8b348
Yusuf Kaka <yusuf@kaka.co.za>
2015-07-10 11:40:33 +0200

    Fixed the example code

--- pytgbotapi/example.py
+++ pytgbotapi/example.py
@@ -2,13 +2,13 @@ from BotApi import BotApi
 
 def main():
     print("Starting...")
-    bot = BotApi("1111111:AAFfsdfsmRsdfdsEFYdsfdsTgdsfTCQ")
+    bot = BotApi("47745865:AAFgsfC2QWsmRNEFYJ3EZPTgBLGrhMMOTCQ")
     print(bot.getMe())
     updates = bot.getUpdates(offset=bot.getLastFetchedId()+1)
-    while len(updates)>0:
-        updates = bot.getUpdates(offset=bot.getLastFetchedId()+1)
-        for update in updates:
-            print(str(update.update_id)+": "+update.message.msg_from.username+": "+update.message.text)
-    bot.sendMessage(72911340,"Hello World")
+    updates = bot.getUpdates(offset=bot.getLastFetchedId()+1)
+    for update in updates:
+        if update.message.msg_from != None:
+            print(str(update.update_id)+": "+str(update.message.msg_from.userid)+": "+update.message.text)
+            bot.sendMessage(str(update.message.msg_from.userid),"Hello World")
 if __name__ == "__main__":
     main()
